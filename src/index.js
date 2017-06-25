927671e3d7390ddaf04fe99132f42f0e9c3a0214
pagnyf <franck.pagny2@gmail.com>
2017-06-25 14:30:06 +0200

    Adding reacteroids

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import { Reacteroids } from './Reacteroids';
+import './style.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Reacteroids />, document.getElementById('root'));
 registerServiceWorker();
