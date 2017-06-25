c12398930322c532f63451c92ee3b89b9d39be97
Foucauld Degeorges <foucdeg@gmail.com>
2017-06-25 16:58:52 +0200

    Integrate server in game

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppContainer from './AppContainer';
 import registerServiceWorker from './registerServiceWorker';
 import { Reacteroids } from './Reacteroids';
 import './style.css';
